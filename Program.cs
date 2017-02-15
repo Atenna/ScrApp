09625d19076c75f51060a27f45ca5d0a855dc82a
Anton Lieskovsky <anton.lieskovsky@gmail.com>
2017-02-15 21:14:40 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using static System.Net.WebProxy;
+
+namespace ScrApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://fri.uniza.sk";
+            WebRequest q = WebRequest.Create(url);
+            q.Proxy = GetDefaultProxy();
+
+            Stream dataStream;
+            dataStream = q.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(dataStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
